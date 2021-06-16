a37c429a94095d6a08a8bedecc75c00ae835e82a
hugoibarrae <72998850+hugoibarrae@users.noreply.github.com>
2021-06-16 13:17:43 -0500

    Add files via upload

--- story_content/user.js
+++ story_content/user.js
@@ -2,23 +2,14 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5sUmEgQthab":
+      case "5foZNfWqJEX":
         Script1();
         break;
-      case "6GmLAmuNNE8":
+      case "5eurDpLS3Sy":
         Script2();
         break;
-      case "65YWu4CN2UF":
+      case "6FBQ5kt0ZIR":
         Script3();
-        break;
-      case "5yvDueXGzht":
-        Script4();
-        break;
-      case "6qqTqdSEda0":
-        Script5();
-        break;
-      case "6Kv1w6cG1sb":
-        Script6();
         break;
   }
 }
@@ -37,70 +28,21 @@ function Script2()
   var player = GetPlayer();
  
 //PLACE YOUR WEB APP URL
-WEB_APP_URL = "Current web app URL";
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbydSbj0MNXxVTOZCHIXXVyp2eMxdr7QSMToYu7Fjaef3ocfLlw/exec";
  
 // STORE ARTICULATE STORYLINE VARIABLES
 // "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
 // ATTENTION: Use a comma if you use multiple Storyline variables
 storyline =
 {
- "date" : new Date().toJSON().slice(0,10), //STORE DATE
- "name" : player.GetVar("name"),
- "email" : player.GetVar("email"),
+ "fecha" : new Date().toJSON().slice(0,10), //STORE DATE
+ "nombre": player.GetVar("var_nombre"),
+ "escuela": player.GetVar("var_escuela"),
  "message" : player.GetVar("message")
 }
 }
 
 function Script3()
-{
-  //DELAY SO JQUERY LIBRARY IS LOADED
-setTimeout(function (){
- 
-//Export to Google
-$.ajax({
-url: WEB_APP_URL,
-type: "POST",
-data : storyline,
-success: function(data)
-{
-console.log(data);
-},
-error: function(err) {
-console.log('Error:', err);
-}
-});
-return false;
-}, 1000);
-}
-
-function Script4()
-{
-  var head = document.getElementsByTagName('head')[0];
-var script = document.createElement('script');
-script.src = '//code.jquery.com/jquery-1.11.0.min.js';
-script.type = 'text/javascript';
-head.appendChild(script)
-}
-
-function Script5()
-{
-  var player = GetPlayer();
- 
-//PLACE YOUR WEB APP URL
-WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwn8lEtAo5OLXK7EWu04x5A9uydaKOoEORmr8pAo4aOyv5l3P4w/exec";
- 
-// STORE ARTICULATE STORYLINE VARIABLES
-// "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
-// ATTENTION: Use a comma if you use multiple Storyline variables
-storyline =
-{
- "Fecha" : new Date().toJSON().slice(0,10), //STORE DATE
- "Nombre" : player.GetVar("Nombre"),
- "Calificacion" : player.GetVar("Results.ScorePercent")
-}
-}
-
-function Script6()
 {
   //DELAY SO JQUERY LIBRARY IS LOADED
 setTimeout(function (){
